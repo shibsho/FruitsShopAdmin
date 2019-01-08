009984d05e4a8da9b6da9d0909dd7ef27154027d
shibsho <shibsho@gmail.com>
2019-01-08 11:14:17 +0900

    Update forms.py

--- apps/sales/forms.py
+++ apps/sales/forms.py
@@ -6,32 +6,6 @@ from .models import Sale
 
 
 class SaleForm(ModelForm):
-	# saled_at = forms.DateTimeField(
-	# 		label = "売上",
-    #         widget=forms.widgets.DateTimeInput(
-    #         	attrs={'type': 'datetime-local',
-	# 					  })
-    #     )
-
-
-	# def __init__(self, *args, **kwargs):
-	# 	super(SaleForm, self).__init__(*args, **kwargs)
-	# 	self.fields['saled_at'].widget = widgets.AdminSplitDateTime()
-
 	class Meta:
 		model = Sale
 		fields = ['item', 'item_num', 'saled_at']
-
-
-	
-		
-"""
-class ShowForm(ModelForm):
-    class Meta:
-        model = Show
-
-    def __init__(self, *args, **kwargs):
-        super(ShowForm, self).__init__(*args, **kwargs)
-        self.fields['start_time'].widget = widgets.AdminSplitDateTime()
-        self.fields['sale_end_time'].widget = widgets.AdminSplitDateTime()
-"""
