34ea1e41a6f54e3b87098f5be645e37bae75bbe5
shibsho <shibsho@gmail.com>
2019-01-15 23:00:02 +0900

    集計処理をmodels.pyからviews.pyに移行

--- apps/sales/views.py
+++ apps/sales/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_POST
-from django.contrib import messages
-from django.core.paginator import Paginator
-from io import TextIOWrapper
 import csv
 import datetime
-from .models import Sale
+from collections import OrderedDict, namedtuple
+from io import TextIOWrapper
+
 from apps.items.models import Item
+from dateutil.relativedelta import relativedelta
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.shortcuts import redirect, render
+from django.utils.timezone import localtime
+from django.views.decorators.http import require_POST
+
 from .forms import SaleForm
+from .models import Sale
 
 
 @login_required
@@ -103,12 +108,140 @@ def csv_upload(request):
 
 @login_required
 def statistics(request):
-    # 全期間
-    entire_sales_amount = Sale.get_entire_amount()
-    # 過去３ヶ月
-    monthly_sale_reports = Sale.get_recent_monthly_reports(3)
-    # 過去３日
-    daily_sale_reports = Sale.get_recent_daily_reports(3)
+    today = datetime.date.today()
+    sales = Sale.objects.all()
+    entire_sales_amount = 0
+
+    """
+    直近3ヶ月分の月間売上情報dict（monthly_sale_reports） は以下の形式
+    {
+        (2018,12):{
+            'amount': 400,
+            'item_reports': {
+                'バナナ': {'item_num': 2, 'amount': 100},
+                'ぶどう': {'item_num': 3, 'amount': 300}
+            }
+        },
+        (2018,11):{
+            'amount': 400,
+            'item_reports': {
+                'バナナ': {'item_num': 2, 'amount': 100},
+                'ぶどう': {'item_num': 3, 'amount': 300}
+            }
+        }
+    }
+    """
+    # 対象月のタプル(yyyy,mm)を作り、monthly_sale_reportsのキーとして設定
+    monthly_sale_reports = OrderedDict()
+    YearMonth = namedtuple('YearMonth', ('year', 'month'))
+    for i in range(3):
+        day = today + relativedelta(months=-i)
+        year_month = YearMonth(
+            year=day.year,
+            month=day.month
+        )
+        monthly_sale_reports[year_month] = {
+            'amount': 0,
+            'item_reports': {}
+        }
+
+    """
+    直近3日分の日間売上情報dict（daily_sale_reports） は以下の形式
+    {
+        (2018,12,31):{
+            'amount': 400,
+            'item_reports': {
+                'バナナ': {'item_num': 2, 'amount': 100},
+                'ぶどう': {'item_num': 3, 'amount': 300}
+            }
+        },
+        (2018,12,30):{
+            'amount': 400,
+            'item_reports': {
+                'バナナ': {'item_num': 2, 'amount': 100},
+                'ぶどう': {'item_num': 3, 'amount': 300}
+            }
+        }
+    }
+    """
+    # 対象日のタプル(yyyy,mm,dd)を作り、daily_sale_reportsのキーとして設定
+    daily_sale_reports = OrderedDict()
+    YearMonthDay = namedtuple('YearMonthDay', ('year', 'month', 'day'))
+    for i in range(3):
+        day = today + relativedelta(days=-i)
+        year_month_day = YearMonthDay(
+            year=day.year,
+            month=day.month,
+            day=day.day
+        )
+        daily_sale_reports[year_month_day] = {
+            'amount': 0,
+            'item_reports': {}
+        }
+
+    # 集計
+    for sale in sales:
+        saled_at_date = localtime(sale.saled_at).date()
+
+        """ 全期間 """
+        entire_sales_amount += sale.amount
+
+        """ 過去3ヶ月間 """
+        # saleの販売日をタプルに変換 => (2018,12)
+        saled_at = YearMonth(
+            year=saled_at_date.year,
+            month=saled_at_date.month,
+        )
+
+        # 対象期間外のsaleであれば何も処理しない
+        if saled_at not in monthly_sale_reports.keys():
+            continue
+
+        # 対象月の売上総額を加算
+        monthly_sale_reports[saled_at]['amount'] += sale.amount
+        # item_reportsにsaleの果物がキーとして存在するとき
+        if (sale.item.name in
+                monthly_sale_reports[saled_at]['item_reports']):
+            (monthly_sale_reports[saled_at]['item_reports']
+             [sale.item.name]['item_num']) += sale.item_num
+            (monthly_sale_reports[saled_at]['item_reports']
+             [sale.item.name]['amount']) += sale.amount
+        # item_reportsにsaleの果物がキーとして存在しないとき
+        else:
+            (monthly_sale_reports[saled_at]
+             ['item_reports'][sale.item.name]) = {
+                'item_num': sale.item_num,
+                'amount': sale.amount
+            }
+        
+        """ 過去3日間 """
+        # saleの販売日をタプルに変換 => (2018,12,31)
+        saled_at = YearMonthDay(
+            year=saled_at_date.year,
+            month=saled_at_date.month,
+            day=saled_at_date.day
+        )
+
+        # 対象期間外のsaleであれば何も処理しない
+        if saled_at not in daily_sale_reports.keys():
+            continue
+
+        # 対象日の売上総額を加算
+        daily_sale_reports[saled_at]['amount'] += sale.amount
+        # item_reportsにsaleの果物がキーとして存在するとき
+        if (sale.item.name in
+                daily_sale_reports[saled_at]['item_reports']):
+            (daily_sale_reports[saled_at]['item_reports']
+             [sale.item.name]['item_num']) += sale.item_num
+            (daily_sale_reports[saled_at]['item_reports']
+             [sale.item.name]['amount']) += sale.amount
+        # item_reportsにsaleの果物がキーとして存在しないとき
+        else:
+            (daily_sale_reports[saled_at]
+             ['item_reports'][sale.item.name]) = {
+                'item_num': sale.item_num,
+                'amount': sale.amount
+            }
 
     return render(request, 'sales/statistics.html', {
         'entire_sales_amount': entire_sales_amount,
